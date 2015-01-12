6e471629586f8149b11e222c2fc5b44ed9c9880a
vzvenyach <vdavez@gmail.com>
2015-01-11 21:44:08 -0500

    updates

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 wget https://github.com/hakimel/reveal.js/archive/2.6.2.zip
 unzip 2.6.2.zip
-mv reveal.js-2.6.2/* reveal
+rm 2.6.2.zip
+mv reveal.js-2.6.2 reveal
+npm install
 ./cli.js test.html > reveal/index.html
+cd reveal
 serve
